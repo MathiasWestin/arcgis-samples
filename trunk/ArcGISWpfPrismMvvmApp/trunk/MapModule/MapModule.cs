afc29922da342c8af448235e7980ff2a5c5b6081
mathias.westin@gmail.com <mathias.westin@gmail.com@a0296a36-7545-5e26-caa4-a9d26354e349>
2011-09-24 08:40:12 +0000

    First release

---
+++ trunk/ArcGISWpfPrismMvvmApp/trunk/MapModule/MapModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+
+namespace MapModule
+{
+    public class MapModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public MapModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.MapView));
+        }
+
+    }
+}
